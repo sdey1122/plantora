1fb291405f5adeb307c74e5ffc0132d0860c0103
Subhankar Dey <subhankardey1122@gmail.com>
2026-08-12 06:32:51 +0530

    auth-issue-solved

--- app/routes/authRoute.js
+++ app/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const authController = require("../controllers/authController");
+const authController = require("../controllers/AuthController");
 
 // const passport = require("../config/passport");
 
